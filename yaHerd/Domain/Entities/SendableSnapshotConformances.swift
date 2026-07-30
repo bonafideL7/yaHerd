5fb5fec2efc56b101852166a00a5ee1083f9f304
bonafideL7 <25960751+bonafideL7@users.noreply.github.com>
2026-07-30 16:11:52 -0500

    Make read snapshots Sendable

---
+++ yaHerd/Domain/Entities/SendableSnapshotConformances.swift
@@ -0,0 +1,47 @@
+import Foundation
+
+extension Sex: Sendable {}
+extension AnimalStatus: Sendable {}
+extension AnimalLocation: Sendable {}
+extension AnimalType: Sendable {}
+extension DistinguishingFeature: Sendable {}
+extension AnimalSummary: Sendable {}
+extension AnimalPregnancyStatus: Sendable {}
+extension PastureOption: Sendable {}
+extension PastureMetrics: Sendable {}
+
+extension DashboardConfiguration: Sendable {}
+extension DashboardAnimalListKind: Sendable {}
+extension DashboardPastureFilter: Sendable {}
+extension DashboardAlertSeverity: Sendable {}
+extension DashboardNavigationTarget: Sendable {}
+extension DashboardPregnancyStatus: Sendable {}
+extension DashboardRecords: Sendable {}
+extension DashboardAnimalRecord: Sendable {}
+extension DashboardHealthRecord: Sendable {}
+extension DashboardPastureRecord: Sendable {}
+extension DashboardWorkingSessionRecord: Sendable {}
+extension DashboardSnapshot: Sendable {}
+extension DashboardAnalytics: Sendable {}
+extension DashboardLifecycleMetric: Sendable {}
+extension DashboardSeasonalCalvingCount: Sendable {}
+extension DashboardCalvingSeason: Sendable {}
+extension DashboardOffspringDamMetric: Sendable {}
+extension DashboardMonthlyMedicalRecordCount: Sendable {}
+extension DashboardYearCount: Sendable {}
+extension DashboardStatusOutcomeYearCount: Sendable {}
+extension DashboardWorkingSessionSummary: Sendable {}
+extension DashboardOverview: Sendable {}
+extension DashboardAnimalItem: Sendable {}
+extension DashboardPastureItem: Sendable {}
+extension DashboardAlert: Sendable {}
+
+extension FieldCheckFindingType: Sendable {}
+extension FieldCheckFindingSeverity: Sendable {}
+extension FieldCheckFindingStatus: Sendable {}
+extension FieldCheckAnimalCheckSnapshot: Sendable {}
+extension FieldCheckFindingSnapshot: Sendable {}
+extension FieldCheckSessionSummary: Sendable {}
+
+extension WorkingTreatmentTemplateSummary: Sendable {}
+extension HomeSnapshot: Sendable {}
