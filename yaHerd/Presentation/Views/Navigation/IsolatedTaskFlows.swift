631edb49a147c5f335b39e4f1bd3eec7bc785660
bonafideL7 <25960751+bonafideL7@users.noreply.github.com>
2026-09-10 19:37:36 -0500

    Refresh sharing access when field check opens

--- yaHerd/Presentation/Views/Navigation/IsolatedTaskFlows.swift
+++ yaHerd/Presentation/Views/Navigation/IsolatedTaskFlows.swift
@@ -31,6 +31,8 @@ extension EnvironmentValues {
 
 struct IsolatedFieldCheckAreaView: View {
     @Environment(AppNavigationState.self) private var navigation
+    @Environment(\.collaborationDependencies) private var collaborationDependencies
+    @Environment(\.appDataAccessMode) private var dataAccessMode
     let onReturnHome: () -> Void
 
     var body: some View {
@@ -41,6 +43,16 @@ struct IsolatedFieldCheckAreaView: View {
                         IsolatedFlowHomeButton(action: onReturnHome)
                     }
                 }
+        }
+        .task {
+            guard dataAccessMode.allowsDataMutations,
+                  let coordinator = collaborationDependencies.syncCoordinator
+            else { return }
+
+            await coordinator.refreshSharingAccessNow(
+                trigger: .screenOpened("Field Check"),
+                minimumInterval: 0
+            )
         }
     }
 
